f5c4526ad4187385dc3fc24808b3154329905b37
Mohammad Kaif <mohdkaif90275@gmail.com>
2025-12-25 21:45:07 +0530

    kaif

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+  })
   return (
     <div>
-        {/* Do not remove the main div */}
+        {posts && posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.id}. {post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
     </div>
   )
 }
